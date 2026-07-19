beeeb472bc1e93bd1d629c6df8d676b76723b841
Esolin BLE <esolinble@gmail.com>
2026-07-19 18:48:09 +0200

    download_json_file function added

---
+++ src/carburants/landing/file_downloader.py
@@ -0,0 +1,18 @@
+import urllib
+import shutil
+import gzip 
+import os
+import urllib.request
+
+
+# download file from URL:https://www.data.gouv.fr/datasets/prix-des-carburants-en-france-flux-instantane-v2-amelioree
+def download_csv_file(url, path):
+    os.makedirs(path, exist_ok=True)
+    file = f"{path}/prix-des-carburants-en-france-flux-instantane-v2.csv"
+    req = urllib.request.Request(url, headers={"Accept-Encoding": "identity"})
+    response = urllib.request.urlopen(req)
+    with gzip.GzipFile(fileobj=response) as gz_in, open(file, "wb") as f_out:
+        shutil.copyfileobj(gz_in, f_out)
+
+def download_json_file():
+    pass
