58d92f53e356f51bf2bcc289a7a4385f821aba19
eble@synchrone.fr <eble@synchrone.fr>
2026-07-24 19:10:21 +0000

    download_csv_file updated : file_path added

--- src/carburants/landing/file_downloader.py
+++ src/carburants/landing/file_downloader.py
@@ -8,14 +8,15 @@ import urllib.request
 # download file from URL:https://www.data.gouv.fr/datasets/prix-des-carburants-en-france-flux-instantane-v2-amelioree
 def download_csv_file(url, path):
     os.makedirs(path, exist_ok=True)
-    file = f"{path}/prix-des-carburants-en-france-flux-instantane-v2.csv"
+    file_path = f"{path}/prix-des-carburants-en-france-flux-instantane-v2.csv"
     req = urllib.request.Request(url, headers={"Accept-Encoding": "identity"})
     response = urllib.request.urlopen(req)
-    with gzip.GzipFile(fileobj=response) as gz_in, open(file, "wb") as f_out:
+    with gzip.GzipFile(fileobj=response) as gz_in, open(file_path, "wb") as f_out:
         shutil.copyfileobj(gz_in, f_out)
+    return file_path
 
-def read_csv_file(path, df_schema, delimiter = ";"):
-    return spark.read.csv(path,schema = df_schema,header = True)
+def read_csv_file(file, df_schema, delimiter = ";"):
+    return spark.read.csv(file,schema = df_schema,header = True)
 
 def download_json_file():
     pass
